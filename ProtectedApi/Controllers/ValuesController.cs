d0cd01ba297e10988dedcaa6fe754bebad4d6dd5
FugenAira-BSIT32E1 <153443814+FugenAira-BSIT32E1@users.noreply.github.com>
2024-06-22 12:23:55 +0800

    add all donee

---
+++ ProtectedApi/Controllers/ValuesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace ProtectedApi
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ValueController : ControllerBase
+    {
+        private readonly string _owner = "Aira M. Fugen";
+        private readonly Random _random = new Random();
+        private readonly string[] _thingsAboutOwner = new[]
+        {
+            "Working on improving patience daily.",
+            "Actively seeking to expand knowledge.",
+        };
+
+        [HttpGet("about/me")]
+        public IActionResult AboutMe()
+        {
+            var thing = _thingsAboutOwner[_random.Next(_thingsAboutOwner.Length)];
+            return Ok(thing);
+        }
+
+        [HttpGet("about")]
+        public IActionResult About()
+        {
+            return Ok(_owner);
+        }
+
+        [HttpPost("about")]
+        public IActionResult About([FromBody] NameModel model)
+        {
+            return Ok($"Hi {model.Name} from {_owner}");
+        }
+    }
+
+    public class NameModel
+    {
+        public string? Name { get; set; }
+    }
+}
