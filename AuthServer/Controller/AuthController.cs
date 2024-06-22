329a6eaa9bb2512e76d98f678e5440da9f2df7c1
FugenAira-BSIT32E1 <153443814+FugenAira-BSIT32E1@users.noreply.github.com>
2024-06-22 11:36:14 +0800

    Add folder and file

---
+++ AuthServer/Controller/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AuthServer.Controller
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PREFINALS_AUTH_fugen_aira_m/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PREFINALS_AUTH_fugen_aira_m.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
